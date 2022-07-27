7c12441b95668ee246abf4ad89659abb7b61ce4b
David Rio Deiros <driodeiros@gmail.com>
2022-07-27 10:46:31 -0400

    remove comments

--- run_backup.sh
+++ run_backup.sh
@@ -7,13 +7,6 @@ PASS_FILE="--password-file=./pass.txt"
 EXCLUDE="--exclude-file=exclude.txt"
 INCLUDE="--files-from=include.txt"
 
-
-# .env
-#B2_BUCKET={{ b2_bucket }}
-#B2_ID={{ b2_account_id }}
-#B2_KEY={{ b2_key }}
-#BUCKET_DIR_NAME={{ b2_dir }}
-
 export B2_ACCOUNT_ID=$B2_ID
 export B2_ACCOUNT_KEY=$B2_KEY
 restic -r b2:$B2_BUCKET:$BUCKET_DIR_NAME backup $INCLUDE $PASS_FILE $EXCLUDE > ${k}.log
