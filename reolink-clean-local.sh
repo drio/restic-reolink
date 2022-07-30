c91a4c5b2860a6709e1dbeb99c700b6455a8aa88
David Rio Deiros <driodeiros@gmail.com>
2022-07-30 13:59:27 -0400

    change dir

--- reolink-clean-local.sh
+++ reolink-clean-local.sh
@@ -6,6 +6,4 @@ if [ ".$1" != "." ];then
   days=$1
 fi
 
-for d in basement driveway garden irongate;do
-  find /home/reolink/$d -type d -ctime +"$days" -exec rm -rf {} \;
-done
+find /home/pi/cameras -type d -ctime +"$days" -exec rm -rf {} \;
