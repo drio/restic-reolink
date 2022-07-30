2b01bf6d0affa84997996fa4783dec6fe864330a
David Rio Deiros <driodeiros@gmail.com>
2022-07-30 14:11:08 -0400

    nc

--- reolink-run-backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-cd $HOME/restic-reolink
-make backup forget
--- run_backup.sh
+++ run_backup.sh
@@ -9,8 +9,10 @@ INCLUDE="--files-from=include.txt"
 
 export B2_ACCOUNT_ID=$B2_ID
 export B2_ACCOUNT_KEY=$B2_KEY
-restic -r b2:$B2_BUCKET backup $INCLUDE $PASS_FILE $EXCLUDE > ${k}.log
+restic --verbose -r b2:$B2_BUCKET backup $INCLUDE $PASS_FILE $EXCLUDE > ${k}.log
 
 ./metrics.sh ./${k}.log type="\"drio-${k}\"" > metrics.$k
 mv ./metrics.$k $NODE_EXPORTER_DIR/metrics.drio.${k}.prom
 rm -f metrics.$k ${k}.log
+
+restic --verbose -r b2:drio-reolink-v2 "--password-file=./pass.txt" forget --prune
